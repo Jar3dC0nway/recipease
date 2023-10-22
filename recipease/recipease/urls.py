142d76481c7f1d295c1322915305a0f86510b209
Jar3dC0nway <hat7wd@virginia.edu>
2023-10-22 04:04:34 -0400

    Renamed folder

--- recipease/recipease/urls.py
+++ recipease/recipease/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for recipease_sub project.
+URL configuration for recipease project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
