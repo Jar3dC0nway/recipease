4c47448876669e963e8e32610843246a5bbceacd
Jar3dC0nway <hat7wd@virginia.edu>
2023-11-03 18:44:51 -0400

    Added multi-ingredient search. Also made it so relevant ingredient tags are highlighted in blue in results so the user knows why certain recipes show up. Also moved the code in views.py for searching from index to search for organization and to make the url RESTful (try /search?search=cheese+onion in the url).

--- recipease/engine/views.py
+++ recipease/engine/views.py
@@ -2,62 +2,81 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
-from .database import sql_return, user_exists, add_user, get_user_info, max_recipeID, add_new_recipe, add_nutrition_info, max_ingredientID, add_ingredient, rating_exists, add_rating, update_rating, add_new_comment, max_commentID
+from .database import sql_return, user_exists, add_user, get_user_info, max_recipeID, add_new_recipe, \
+    add_nutrition_info, max_ingredientID, add_ingredient, rating_exists, add_rating, update_rating, add_new_comment, \
+    max_commentID
 from .forms import SearchForm, RecipeForm, IngredientForm, IngredientFormSet, RecipeRatingForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user
+
 
 def index(request):
     user = get_user(request)  # Get the authenticated user
     if user.is_authenticated:
         if not user_exists(user.email):
             add_user(user.email, user.username)
-    if request.method == "POST":
-        form = SearchForm(request.POST, request.FILES)
-        if form.is_valid():
-            search_term = request.POST.get('search')
-            ingredient_search = (f"SELECT recipeID "
-                                 f"FROM Recipe NATURAL JOIN Recipe_Ingredients "
-                                 f"NATURAL JOIN Ingredient "
-                                 f"WHERE Ingredient.name LIKE '%{search_term}%'")
-            full_search = (f"SELECT * "
-                           f"FROM Recipe NATURAL JOIN Ingredient NATURAL JOIN Recipe_Ingredients "
-                           f"WHERE Recipe.recipeID IN ({ingredient_search});")
-            search_result = sql_return(full_search)
-
-            cleaned = []
-            last = ""
-            for s in search_result[0]:
-                if s[3] != last:
-                    last = s[3]
-                    cleaned.append(s)
-                else:
-                    l = list(cleaned[-1])
-                    l[8] = l[8] + ", " + s[8]
-                    cleaned[-1] = tuple(l)
-
-            print(cleaned)
-
-            return render(request, 'search.html', {'search': cleaned})
 
     form = SearchForm()
     return render(request, 'index.html', {'form': form})
 
+
 def search(request):
+    if request.method != "GET":
+        return HttpResponseRedirect('index')
+    form = SearchForm(request.GET, request.FILES)
+    if form.is_valid():
+        search_term = str(request.GET.get('search'))
+        terms = search_term.split(" ")
+        ingredient_search = "SELECT recipeID FROM Recipe WHERE false"  # Start with empty table
+        for term in terms:
+            ingredient_search += (f" UNION (SELECT recipeID "  # Add each term to recipe search
+                                  f"FROM Recipe NATURAL JOIN Recipe_Ingredients "
+                                  f"NATURAL JOIN Ingredient "
+                                  f"WHERE Ingredient.name LIKE '%{term}%')")
+        full_search = (f"SELECT * "  # Convert recipeIDs back into recipe data
+                       f"FROM Recipe NATURAL JOIN Ingredient NATURAL JOIN Recipe_Ingredients "
+                       f"WHERE Recipe.recipeID IN ({ingredient_search});")
+        search_result = sql_return(full_search)
+
+        cleaned = []
+        last = ""
+        for s in search_result[0]:  # Remove duplicate recipeIDs and combine ingredient data
+            if s[3] != last:  # If you haven't seen this recipe yet, add it
+                last = s[3]
+                cleaned.append(s)
+                li = list(cleaned[-1])
+                li[8] = [s[8]]
+            else:  # Otherwise, don't add it and just add the ingredient info
+                li = list(cleaned[-1])
+                li[8].append(s[8])
+            cleaned[-1] = tuple(li)
+
+            for term in terms:  # Make it so matching ingredients will become highlighted in the search results page
+                if term in li[8][-1]:
+                    if term != li[8][-1]:
+                        terms.append(li[8][-1])
+
+        # print(cleaned)
+
+        return render(request, 'search.html', {'search': cleaned, 'terms': terms})
+
     return render(request, "search.html")
 
+
 @login_required
-def view_profile(request, owner = None):
+def view_profile(request, owner=None):
     curr_user = get_user(request)
     if owner:
-        owner_user = get_user_info(owner) #returns username
+        owner_user = get_user_info(owner)  # returns username
     else:
         owner_user = curr_user
     return render(request, 'profile.html',
-        {"request_type":request.method, "user":curr_user, "owner":owner_user,})
+                  {"request_type": request.method, "user": curr_user, "owner": owner_user, })
+
 
 def success_view(request):
-    return render(request, 'success.html') 
+    return render(request, 'success.html')
+
 
 @login_required
 def add_recipe(request):
@@ -66,16 +85,16 @@ def add_recipe(request):
         ingredients_formset = IngredientFormSet(request.POST)
 
         if form.is_valid() and ingredients_formset.is_valid():
-            recipe_id = max_recipeID() + 1  
+            recipe_id = max_recipeID() + 1
             title = form.cleaned_data['title']
             description = form.cleaned_data['description']
             cook_time = form.cleaned_data['cook_time']
             instructions = form.cleaned_data['instructions']
 
-            #add to Recipe table
+            # add to Recipe table
             add_new_recipe(get_user(request).email, recipe_id, title, description, cook_time, instructions)
 
-            #add to Nutrition table
+            # add to Nutrition table
             calories = form.cleaned_data['calories']
             fat = form.cleaned_data['fat']
             sat_fat = form.cleaned_data['sat_fat']
@@ -83,19 +102,18 @@ def add_recipe(request):
             fiber = form.cleaned_data['fiber']
             sugar = form.cleaned_data['sugar']
             protein = form.cleaned_data['protein']
-            add_nutrition_info(recipe_id, calories , fat, sat_fat, carbs, fiber, sugar, protein)
+            add_nutrition_info(recipe_id, calories, fat, sat_fat, carbs, fiber, sugar, protein)
 
-          
             for ingredient_form in ingredients_formset:
                 name = ingredient_form.cleaned_data.get('name')
                 amount = ingredient_form.cleaned_data.get('amount')
                 ingredient_type = ingredient_form.cleaned_data.get('ingredient_type')
-                ingredientID = max_ingredientID() + 1 
+                ingredientID = max_ingredientID() + 1
 
-                #add to DB 
+                # add to DB
                 add_ingredient(ingredientID, recipe_id, name, ingredient_type, amount)
 
-            return redirect('success_view') 
+            return redirect('success_view')
 
     else:
         form = RecipeForm()
@@ -103,19 +121,21 @@ def add_recipe(request):
 
     return render(request, 'add_recipe.html', {'form': form, 'ingredients_formset': ingredients_formset})
 
+
 def rating_success_view(request):
     return render(request, 'rating_success.html')
+
 
 @login_required
 def rate_recipe(request, recipe_id):
     if request.method == 'POST':
-        rating = request.POST.get('rating') 
+        rating = request.POST.get('rating')
         form = RecipeRatingForm(request.POST)
 
         if form.is_valid():
             email = get_user(request).email
             rating = form.cleaned_data['rating']
-            
+
             # Check if the user has already rated this recipe
             exists = rating_exists(email, recipe_id)
             print(exists)
@@ -127,12 +147,14 @@ def rate_recipe(request, recipe_id):
                 # Create a new rating
                 add_rating(email, recipe_id, rating)
 
-            return redirect('rating_success_view') 
+            return redirect('rating_success_view')
 
-    return render(request, 'rate_recipe.html', {'recipe_id': recipe_id}) 
+    return render(request, 'rate_recipe.html', {'recipe_id': recipe_id})
+
 
 def comment_success_view(request):
     return render(request, 'comment_success.html')
+
 
 @login_required
 def add_comment(request, recipe_id):
@@ -144,9 +166,8 @@ def add_comment(request, recipe_id):
             commentID = max_commentID() + 1
 
             # Insert the new comment using custom SQL
-            add_new_comment(email, recipe_id, content, commentID )
+            add_new_comment(email, recipe_id, content, commentID)
 
-            return redirect('comment_success_view') 
+            return redirect('comment_success_view')
 
     return render(request, 'add_comment.html', {'recipe_id': recipe_id})
-
